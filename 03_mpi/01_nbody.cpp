3b6d598bb300df74b940a9b9bacf547c62759396
saitotm <tomokazu.ml.tech@gmail.com>
2023-04-20 16:04:16 +0900

    Distribute Nbody problem calculation with Gather

--- 03_mpi/01_nbody.cpp
+++ 03_mpi/01_nbody.cpp
@@ -1,18 +1,37 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
+#include <mpi.h>
 
 int main() {
   const int N = 20;
   //const int np = 4;
-  double x[N], y[N], m[N], fx[N], fy[N];
-  for(int i=0; i<N; i++) {
+  double x0[N], y0[N], m0[N], fx0[N], fy0[N];
+
+  MPI_Init(NULL, NULL);
+  int size, rank;
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  int begin = rank * (N / size);
+  int end = (rank + 1) * (N / size);
+
+  srand48(rank);
+  for(int i=begin; i<end; i++) {
     //if(i % (N / np) == 0) srand48(i / (N / np));
-    x[i] = drand48();
-    y[i] = drand48();
-    m[i] = drand48();
-    fx[i] = fy[i] = 0;
+    x0[i] = drand48();
+    y0[i] = drand48();
+    m0[i] = drand48();
+    fx0[i] = fy0[i] = 0;
   }
+
+  double x[N], y[N], m[N], fx[N], fy[N];
+  MPI_Gather(x0 + begin, end - begin, MPI_DOUBLE, x, end - begin, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  MPI_Gather(y0 + begin, end - begin, MPI_DOUBLE, y, end - begin, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  MPI_Gather(m0 + begin, end - begin, MPI_DOUBLE, m, end - begin, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  MPI_Gather(fx0 + begin, end - begin, MPI_DOUBLE, fx, end - begin, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  MPI_Gather(fy0 + begin, end - begin, MPI_DOUBLE, fy, end - begin, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+  if (rank == 0) {
   for(int i=0; i<N; i++) {
     for(int j=0; j<N; j++) {
       if(i != j) {
@@ -23,6 +42,12 @@ int main() {
         fy[i] -= ry * m[j] / (r * r * r);
       }
     }
-    printf("%d %g %g\n",i,fx[i],fy[i]);
+    //printf("%d %g %g\n",i,fx[i],fy[i]);
   }
+
+      for (int i = 0; i < N; i++) {
+        printf("%d %g %g\n",i,fx[i],fy[i]);
+      }
+  }
+  MPI_Finalize();
 }
